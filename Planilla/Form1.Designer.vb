7d5935efb149c9831832aadb71bf11d23701233e
DAVID_TOJ\da390 <da3909622@gmail.com>
2023-05-11 02:46:03 -0600

    Validate if the information is already exist or not. If exist show a error message If not exist code, show succesfully message

--- Planilla/Form1.Designer.vb
+++ Planilla/Form1.Designer.vb
@@ -22,10 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(346, 21)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(80, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Proyecto final"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(28, 110)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Ver planilla"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(129, 110)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(226, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "Ingresar nuevos empleados"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(387, 110)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(141, 23)
+        Button3.TabIndex = 3
+        Button3.Text = "Generar cheque"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
